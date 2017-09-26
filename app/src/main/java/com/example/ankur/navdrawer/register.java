070a936dd0a74407ab7d853322b89493fee4d8d3
Ankur-singh <as.ankursingh3.1@gmail.com>
2017-09-26 15:25:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ankur/navdrawer/register.java
@@ -0,0 +1,35 @@
+package com.example.ankur.navdrawer;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link register.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link register#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class register extends Fragment {
+
+    public register() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+
+}
